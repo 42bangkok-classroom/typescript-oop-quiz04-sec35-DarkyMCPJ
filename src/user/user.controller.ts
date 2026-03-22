f17707f2f71506aadd1394cafbf805ab581f6689
DarkyMCPJ <harumi.ploy@gmail.com>
2026-03-22 14:25:13 +0700

    02

---
+++ src/user/user.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly UserService: UserService) {}
+
+  @Get()
+  test(): string {
+    return this.UserService.test();
+  }
+}
