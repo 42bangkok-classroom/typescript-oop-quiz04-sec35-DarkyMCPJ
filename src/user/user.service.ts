a086fcfae1145895e3a20abeeb50a9b9488f64fe
DarkyMCPJ <harumi.ploy@gmail.com>
2026-03-24 13:59:01 +0700

    04

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { IUser } from './user.interface';
-
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
@@ -16,5 +15,28 @@ export class UserService {
     const readUserData = readFileSync(filePath, 'utf-8');
     const userData = JSON.parse(readUserData) as IUser[];
     return userData;
+  }
+
+
+  findOne(id: string, fields?: string[]): Partial<InterUser> {
+    const users = this.findAll();
+    const user = users.find((u) => u.id === id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    if (fields) {
+      const filteredUser: Partial<InterUser> = {};
+
+      fields.forEach((field) => {
+        if (user[field as keyof InterUser] !== undefined) {
+          filteredUser[field as keyof InterUser] =
+            user[field as keyof InterUser];
+        }
+      });
+      return filteredUser;
+    }
+    return user;
   }
 }
